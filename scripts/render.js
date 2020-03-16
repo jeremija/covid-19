1ef44f491130e2ed0009e6d22960769e05ce3f83
Jerko Steiner <jerko.steiner@gmail.com>
2020-03-16 11:14:16 +0100

    Add basic project structure

---
+++ scripts/render.js
@@ -0,0 +1,19 @@
+const ejs = require('ejs')
+const fs = require('fs')
+const path = require('path')
+const data = require('../data')
+
+const outDir = path.join(__dirname, '..', 'build')
+fs.mkdirSync(outDir, {recursive: true})
+
+const files = process.argv.slice(2)
+
+files.forEach(file => {
+  const source = fs.readFileSync(file, 'utf8')
+  const basename = path.basename(file)
+  console.log('render', basename)
+  const outFile = path.join(outDir, basename)
+  const t = ejs.compile(source, {async: false})
+  const result = t(data[basename])
+  fs.writeFileSync(outFile, result)
+})
